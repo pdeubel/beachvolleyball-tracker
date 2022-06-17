5e658a2a70c70b888ed22b6be6a21e2f9ddf3604
Patrick Deubel <patrick.deubel@gmail.com>
2022-06-17 16:01:37 +0200

    Database Schema

--- backend/database_schema.py
+++ backend/database_schema.py
@@ -9,17 +9,17 @@ class Player(db.Model, UserMixin):
 
     __tablename__ = "player"
 
-    user_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    username = db.Column(db.String(80), nullable=True)  # Filled in by the user
+    player_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(254), unique=True, nullable=False)
+    player_name = db.Column(db.String(80), nullable=True)  # Filled in by the user
 
     def __repr__(self):
-        if repr(self.username) is None:
+        if repr(self.player_name) is None:
             return f"<User {repr(self.email)}>"
-        return f"<User {repr(self.username)}>"
+        return f"<User {repr(self.player_name)}>"
 
     def get_id(self):
-        return str(self.user_id)
+        return str(self.player_id)
 
 
 class Game(db.Model):
@@ -27,16 +27,20 @@ class Game(db.Model):
     __tablename__ = "game"
 
     game_id = db.Column(db.Integer, primary_key=True)
+    team_0_won = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
-        return f"<Game #{repr(self.game_id)}>"
+        return f"<Game #{repr(self.game_id)} -- Team {1 if self.team_0_won else 2} won>"
 
     def get_id(self):
         return str(self.game_id)
 
 
-class PlayersInGame(db.Model):
+class GamesAndPlayers(db.Model):
 
     __tablename__ = "players_in_game"
 
-
+    games_and_players_id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey("game.game_id"), nullable=False)
+    player_id = db.Column(db.Integer, db.ForeignKey("player.player_id"), nullable=False)
+    team = db.Column(db.Boolean, nullable=False)
