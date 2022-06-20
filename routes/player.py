92098973e03c9cdd64239347f1a9704de4ba1017
Patrick Deubel <patrick.deubel@gmail.com>
2022-06-20 15:05:17 +0200

    Player Site: Added form to enter username if not present, added QR code
    - Username is checked, if not it needs to be entered
    - QR Code is a json dict encoded in base64 of the playerid and email

--- routes/player.py
+++ routes/player.py
@@ -1,10 +1,62 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+import json
+from base64 import b64encode
+from io import BytesIO
+
+import qrcode
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField
+
+from backend.database_schema import db
 
 player_page = Blueprint("player", __name__)
 
 
-@player_page.route("/player", methods=["GET"])
+class PlayerNameForm(FlaskForm):
+    player_name = StringField("Spielername")
+
+
+def convert_pil_image_to_bytes(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return img_io
+
+
+@player_page.route("/player", methods=["GET", "POST"])
 @login_required
 def player_site():
-    return render_template("player_site.html")
+    player_name = current_user.player_name
+
+    if player_name is None:
+        form = PlayerNameForm()
+
+        if form.validate_on_submit():
+            player_name = form.player_name.data
+
+            current_user.player_name = player_name
+            db.session.commit()
+
+            return redirect(url_for("player.player_site"))
+
+        return render_template("player_site.html", form=form)
+
+    qr = qrcode.QRCode(
+        version=8,
+        box_size=4,
+        border=4
+    )
+
+    to_encode = {
+        "player_id": current_user.player_id,
+        "email": current_user.email
+    }
+
+    qr.add_data(b64encode(json.dumps(to_encode).encode("ascii")))
+    qr.make(fit=True)
+
+    image_io = convert_pil_image_to_bytes(qr.make_image(fill_color="black", back_color="white"))
+    player_qr_code_b64 = b64encode(image_io.getvalue()).decode("ascii")
+
+    return render_template("player_site.html", player_name=player_name, player_qr_code_b64=player_qr_code_b64)
