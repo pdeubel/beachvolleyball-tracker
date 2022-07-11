86475cb36d7fab84186814b33c705788645d1c4e
Patrick Deubel <patrick.deubel@gmail.com>
2022-07-11 16:06:30 +0200

    Fixed setting TLS for mail server by env variable
    - Did not work because env variable is always parsed as string and not bool

--- mod_beach.py
+++ mod_beach.py
@@ -58,7 +58,7 @@ def create_app():
     app.config["MAIL_PORT"] = os.getenv("MAIL_PORT", "25")
     app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME", mail_username)
     app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD", mail_password)
-    app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", True)
+    app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "False").lower() in ("true", "1")
 
     # Database app initialization
     db.init_app(app)
